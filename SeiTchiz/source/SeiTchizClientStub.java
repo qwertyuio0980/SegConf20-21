ed421f51a2b2d6ce98c85410fcb04388844f251e
fc51304 <fc51304@alunos.fc.ul.pt>
2021-02-11 17:08:49 +0000

    Update SeiTchizClientStub.java

--- SeiTchiz/source/SeiTchizClientStub.java
+++ SeiTchiz/source/SeiTchizClientStub.java
@@ -1,3 +1,20 @@
 public class SeiTchizClientStub {
 
+    public void conectarServidor(String serverAdress) {
+
+    }
+
+    public void conectarServidor(String serverAdress, String clientID) {
+
+    }
+
+    public void conectarServidor(String serverAdress, String clientID) {
+
+    }
+
+    public void conectarServidor(String serverAdress, String clientID, ) {
+
+    }
+
+
 }
